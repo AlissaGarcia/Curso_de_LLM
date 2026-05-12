7dac7f765874142d08613e74f0bfa0d7583517cc
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-05-12 17:46:22 +0000

    Criação de ambiente virtual e interpretador de imagens simples

---
+++ Atividade05/main.py
@@ -0,0 +1,19 @@
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+#for modelos in client.models.list():
+   #python print(f"Modelos : {modelos.name}")
+
+image_path = 'img/image.png'
+
+image_file = client.files.upload(file=image_path)
+
+response = client.models.generate_content(
+    model = 'gemini-2.5-flash',
+    config = types.GenerateContentConfig(system_instruction ='Seu nome é Carlinhos e você é amigável.'),
+    contents = [image_file, 'O que você acha dessa imagem?']
+)
+
+print(response.text)
